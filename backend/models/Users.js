0bcae9b7493ae0b8e4883ead1876c87b67910279
toxiic-me <iamtoxiicman@gmail.com>
2024-03-31 16:16:41 +0530

    payment added

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    payments: {
+        type: [Object],
+        default: []
+    },
     orders: {
         type: Array,
         default: []
